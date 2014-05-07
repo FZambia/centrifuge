9703616b1c222198ccb4d556d222cd1f14486a1d
FZambia <frvzmb@gmail.com>
2014-05-07 10:37:09 +0400

    0.5.2dev

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ def long_description():
 
 setup(
     name='centrifuge',
-    version='0.5.1',
+    version='0.5.2dev',
     description="Simple real-time messaging in web applications",
     long_description=long_description(),
     url='https://github.com/FZambia/centrifuge',
