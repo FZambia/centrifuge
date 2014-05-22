137a1a533539ed633b875791388c2b9f6e259488
Alexandr Emelin <frvzmb@gmail.com>
2014-05-22 18:50:51 +0400

    fix css

--- examples/weather/core/models.py
+++ examples/weather/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Data(models.Model):
 
-    weather_id = models.CharField("weather id", max_length=10)
+    weather_id = models.IntegerField("weather id")
     weather_type = models.CharField("weather type", max_length=30)
     weather_description = models.CharField("weather description", max_length=255)
     temperature = models.FloatField("temperature")
